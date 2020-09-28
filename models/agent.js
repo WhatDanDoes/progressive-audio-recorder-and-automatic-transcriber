18f6020cc985a2e6c0f638c3334ea22ad143f6a7
Daniel Bidulock <daniel_bidulock@sil.org>
2020-09-28 15:28:16 -0600

    Remove password stuff

--- models/agent.js
+++ models/agent.js
@@ -24,14 +24,14 @@ module.exports = function(mongoose) {
         message: 'That email is already registered'
       }
     },
-    password: {
-      type: String,
-      trim: true,
-      required: [true, 'No password supplied'],
-      empty: [false, 'No password supplied'],
-    },
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
+//    password: {
+//      type: String,
+//      trim: true,
+//      required: [true, 'No password supplied'],
+//      empty: [false, 'No password supplied'],
+//    },
+//    resetPasswordToken: String,
+//    resetPasswordExpires: Date,
     canRead: [{ type: Schema.Types.ObjectId, ref: 'Agent', unique: true }],
   }, {
     timestamps: true
@@ -39,36 +39,36 @@ module.exports = function(mongoose) {
 
   const saltRounds = 10;
 
-  AgentSchema.pre('save', function(next) {
-    // Check if document is new or a new password has been set
-    if (this.isNew || this.isModified('password')) {
-      // Saving reference to this because of changing scopes
-      const document = this;
-      bcrypt.hash(document.password, saltRounds,
-        function(err, hashedPassword) {
-        if (err) {
-          next(err);
-        }
-        else {
-          document.password = hashedPassword;
-          next();
-        }
-      });
-    } else {
-      next();
-    }
-  });
-
-  AgentSchema.statics.validPassword = function(password, hash, done, agent) {
-    bcrypt.compare(password, hash, function(err, isMatch) {
-      if (err) console.log(err);
-      if (isMatch) {
-        return done(null, agent);
-      } else {
-        return done(null, false);
-      }
-    });
-  };
+//  AgentSchema.pre('save', function(next) {
+//    // Check if document is new or a new password has been set
+//    if (this.isNew || this.isModified('password')) {
+//      // Saving reference to this because of changing scopes
+//      const document = this;
+//      bcrypt.hash(document.password, saltRounds,
+//        function(err, hashedPassword) {
+//        if (err) {
+//          next(err);
+//        }
+//        else {
+//          document.password = hashedPassword;
+//          next();
+//        }
+//      });
+//    } else {
+//      next();
+//    }
+//  });
+//
+//  AgentSchema.statics.validPassword = function(password, hash, done, agent) {
+//    bcrypt.compare(password, hash, function(err, isMatch) {
+//      if (err) console.log(err);
+//      if (isMatch) {
+//        return done(null, agent);
+//      } else {
+//        return done(null, false);
+//      }
+//    });
+//  };
 
   AgentSchema.methods.getAgentDirectory = function() {
     let parts = this.email.split('@');
