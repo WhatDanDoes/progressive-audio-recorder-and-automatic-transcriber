cff56031d859a9f15c395f3006fd8d56579a2639
Daniel Bidulock <daniel_bidulock@sil.org>
2020-09-29 07:02:37 -0600

    Remove tests for deprecated functionality

--- spec/models/agentSpec.js
+++ spec/models/agentSpec.js
@@ -15,10 +15,10 @@ describe('Agent', function() {
     db.mongoose.connection.db.dropDatabase().then(function(result) {
       done();
     }).catch(function(err) {
-      done.fail(err);         
+      done.fail(err);
     });
   });
- 
+
   describe('basic validation', function() {
     it('sets the createdAt and updatedAt fields', function(done) {
       expect(agent.createdAt).toBe(undefined);
@@ -31,24 +31,24 @@ describe('Agent', function() {
         done.fail(err);
       });
     });
-  
-    it("encrypts the agent's password", function(done) {
-      expect(agent.password).toEqual('secret');
-      agent.save().then(function(obj) {
-        Agent.findById(obj._id).then(function(results) {
-          expect(results.password).not.toEqual('secret');
-          done();
-        }).catch(err => {
-          done.fail(err);
-        });
-      }).catch(err => {
-        done.fail(err);
-      });
-    });
+
+//    it("encrypts the agent's password", function(done) {
+//      expect(agent.password).toEqual('secret');
+//      agent.save().then(function(obj) {
+//        Agent.findById(obj._id).then(function(results) {
+//          expect(results.password).not.toEqual('secret');
+//          done();
+//        }).catch(err => {
+//          done.fail(err);
+//        });
+//      }).catch(err => {
+//        done.fail(err);
+//      });
+//    });
 
     it('does not allow two identical emails', function(done) {
       agent.save().then(function(obj) {
-        Agent.create({ email: 'someguy@example.com', password: 'secret' }).then(function(obj) {
+        Agent.create({ email: 'someguy@example.com' }).then(function(obj) {
           done.fail('This should not have saved');
         }).catch(function(error) {
           expect(Object.keys(error.errors).length).toEqual(1);
@@ -61,7 +61,7 @@ describe('Agent', function() {
     });
 
     it('does not allow an empty email field', function(done) {
-      Agent.create({ email: ' ', password: 'secret' }).then(function(obj) {
+      Agent.create({ email: ' ' }).then(function(obj) {
         done.fail('This should not have saved');
       }).catch(function(error) {
         expect(Object.keys(error.errors).length).toEqual(1);
@@ -71,7 +71,7 @@ describe('Agent', function() {
     });
 
     it('does not allow an undefined email field', function(done) {
-      Agent.create({ password: 'secret' }).then(function(obj) {
+      Agent.create({ }).then(function(obj) {
         done.fail('This should not have saved');
       }).catch(function(error) {
         expect(Object.keys(error.errors).length).toEqual(1);
@@ -80,36 +80,36 @@ describe('Agent', function() {
       });
     });
 
-    it('does not allow an empty password field', function(done) {
-      Agent.create({ email: 'someguy@example.com', password: '   ' }).then(function(obj) {
-        done.fail('This should not have saved');
-      }).catch(function(error) {
-        expect(Object.keys(error.errors).length).toEqual(1);
-        expect(error.errors['password'].message).toEqual('No password supplied');
-        done();
-      });
-    });
-
-    it('does not allow an undefined password field', function(done) {
-      Agent.create({ email: 'someguy@example.com' }).then(function(obj) {
-        done.fail('This should not have saved');
-      }).catch(function(error) {
-        expect(Object.keys(error.errors).length).toEqual(1);
-        expect(error.errors['password'].message).toEqual('No password supplied');
-        done();
-      });
-    });
-
-    it('does not re-hash a password on update', function(done) {
-      agent.save().then(function(obj) {
-        var passwordHash = agent.password;
-        agent.email = 'newemail@example.com';
-        agent.save().then(function(obj) {
-          expect(agent.password).toEqual(passwordHash); 
-          done();
-        });
-      });
-    });
+//    it('does not allow an empty password field', function(done) {
+//      Agent.create({ email: 'someguy@example.com', password: '   ' }).then(function(obj) {
+//        done.fail('This should not have saved');
+//      }).catch(function(error) {
+//        expect(Object.keys(error.errors).length).toEqual(1);
+//        expect(error.errors['password'].message).toEqual('No password supplied');
+//        done();
+//      });
+//    });
+//
+//    it('does not allow an undefined password field', function(done) {
+//      Agent.create({ email: 'someguy@example.com' }).then(function(obj) {
+//        done.fail('This should not have saved');
+//      }).catch(function(error) {
+//        expect(Object.keys(error.errors).length).toEqual(1);
+//        expect(error.errors['password'].message).toEqual('No password supplied');
+//        done();
+//      });
+//    });
+//
+//    it('does not re-hash a password on update', function(done) {
+//      agent.save().then(function(obj) {
+//        var passwordHash = agent.password;
+//        agent.email = 'newemail@example.com';
+//        agent.save().then(function(obj) {
+//          expect(agent.password).toEqual(passwordHash);
+//          done();
+//        });
+//      });
+//    });
 
     /**
      * canRead relationship
@@ -153,14 +153,14 @@ describe('Agent', function() {
 
         let viewableAgent = new Agent({ email: 'vieweableAgent@example.com', password: 'secret' });
         viewableAgent.save().then(function(result) {
-        
+
           agent.canRead.push(viewableAgent._id);
           newAgent.canRead.push(viewableAgent._id);
 
           agent.save().then(function(result) {
             expect(agent.canRead.length).toEqual(1);
             expect(agent.canRead[0]).toEqual(viewableAgent._id);
-  
+
             newAgent.save().then(function(result) {
               expect(newAgent.canRead.length).toEqual(1);
               expect(newAgent.canRead[0]).toEqual(viewableAgent._id);
@@ -216,30 +216,30 @@ describe('Agent', function() {
     /**
      * .validPassword
      */
-    describe('.validPassword', function() {
-      beforeEach(function(done) {
-        agent.save().then(function(obj) {
-          done();
-        });
-      });
-
-      it('returns true if the password is a match', function(done) {
-        Agent.validPassword('secret', agent.password, function(err, res) {
-          expect(res).toEqual(agent);
-          done();
-        }, agent);
-      });
-
-      it('returns false if the password is not a match', function(done) {
-        Agent.validPassword('wrongsecretpassword', agent.password, function(err, res) {
-          expect(res).toBe(false);
-          done();
-        }, agent);
-      });
-    });
+//    describe('.validPassword', function() {
+//      beforeEach(function(done) {
+//        agent.save().then(function(obj) {
+//          done();
+//        });
+//      });
+//
+//      it('returns true if the password is a match', function(done) {
+//        Agent.validPassword('secret', agent.password, function(err, res) {
+//          expect(res).toEqual(agent);
+//          done();
+//        }, agent);
+//      });
+//
+//      it('returns false if the password is not a match', function(done) {
+//        Agent.validPassword('wrongsecretpassword', agent.password, function(err, res) {
+//          expect(res).toBe(false);
+//          done();
+//        }, agent);
+//      });
+//    });
 
     /**
-     * .validPassword
+     * .getAgentDirectory
      */
     describe('.getAgentDirectory', function() {
       it('returns a directory path based on the agent\'s email address', () => {
