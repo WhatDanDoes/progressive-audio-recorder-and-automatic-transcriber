b60107eb6ae8ef8839e8df9ae31c24c22e3b7fa3
Daniel Bidulock <daniel_bidulock@sil.org>
2020-10-02 14:05:20 -0600

    Get streamlined image deletion tests

--- spec/features/imageDeleteSpec.js
+++ spec/features/imageDeleteSpec.js
@@ -30,7 +30,7 @@ const mockAndUnmock = require('../support/mockAndUnmock')(mock);
 // For when system resources are scarce
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
-describe('DELETE /image/:domain/:agentId/:imageId', function() {
+describe('Deleting an image', () => {
 
   let browser, agent, lanny;
 
@@ -64,194 +64,326 @@ describe('DELETE /image/:domain/:agentId/:imageId', function() {
     });
   });
 
-  describe('unauthenticated', function() {
-    it('does not allow deleting an image', function(done) {
-      request(app)
-        .delete(`/image/${agent.getAgentDirectory()}/image2.jpg`)
-        .end(function(err, res) {
-          if (err) return done.fail(err);
-          expect(res.status).toEqual(302);
-          expect(res.header.location).toEqual('/');
-          done();
-        });
+  describe('from show view', function() {
+
+    describe('unauthenticated', function() {
+      it('does not allow deleting an image', function(done) {
+        request(app)
+          .delete(`/image/${agent.getAgentDirectory()}/image2.jpg`)
+          .end(function(err, res) {
+            if (err) return done.fail(err);
+            expect(res.status).toEqual(302);
+            expect(res.header.location).toEqual('/');
+            done();
+          });
+      });
     });
-  });
 
-  describe('authenticated', function() {
-    beforeEach(done => {
+    describe('authenticated', function() {
+      beforeEach(done => {
 
-      stubAuth0Sessions(agent.email, DOMAIN, err => {
-        if (err) done.fail(err);
-
-        // Don't mock too soon. The stub needs some files
-        mockAndUnmock({
-          [`uploads/${agent.getAgentDirectory()}`]: {
-            'image1.jpg': fs.readFileSync('spec/files/troll.jpg'),
-            'image2.jpg': fs.readFileSync('spec/files/troll.jpg'),
-            'image3.jpg': fs.readFileSync('spec/files/troll.jpg'),
-          },
-          [`uploads/${lanny.getAgentDirectory()}`]: {
-            'lanny1.jpg': fs.readFileSync('spec/files/troll.jpg'),
-            'lanny2.jpg': fs.readFileSync('spec/files/troll.jpg'),
-            'lanny3.jpg': fs.readFileSync('spec/files/troll.jpg'),
-          },
-          'public/images/uploads': {}
-        });
-
-        browser.clickLink('Login', function(err) {
+        stubAuth0Sessions(agent.email, DOMAIN, err => {
           if (err) done.fail(err);
+
+          // Don't mock too soon. The stub needs some files
+          mockAndUnmock({
+            [`uploads/${agent.getAgentDirectory()}`]: {
+              'image1.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'image2.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'image3.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            },
+            [`uploads/${lanny.getAgentDirectory()}`]: {
+              'lanny1.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'lanny2.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'lanny3.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            },
+            'public/images/uploads': {}
+          });
+
+          browser.clickLink('Login', function(err) {
+            if (err) done.fail(err);
+            browser.assert.success();
+            done();
+          });
+        });
+      });
+
+      afterEach(() => {
+        mock.restore();
+      });
+
+      it('renders a form to allow an agent to delete an image', function(done) {
+        browser.clickLink(`a[href="/image/${agent.getAgentDirectory()}/image1.jpg"]`, (err) => {
+          if (err) return done.fail(err);
           browser.assert.success();
+          browser.assert.element('#delete-image-form');
+          browser.assert.element(`form[action="/image/${agent.getAgentDirectory()}/image1.jpg?_method=DELETE"]`);
           done();
         });
       });
-    });
 
-    afterEach(() => {
-      mock.restore();
-    });
-
-    it('renders a form to allow an agent to delete an image', function(done) {
-      browser.clickLink(`a[href="/image/${agent.getAgentDirectory()}/image1.jpg"]`, (err) => {
-        if (err) return done.fail(err);
-        browser.assert.success();
-        browser.assert.element('#delete-image-form');
-        browser.assert.element(`form[action="/image/${agent.getAgentDirectory()}/image1.jpg?_method=DELETE"]`);
-        done();
-      });
-    });
-
-    describe('deleting', function() {
-      describe('owner resource', function() {
-        beforeEach(function(done) {
-          browser.clickLink(`a[href="/image/${agent.getAgentDirectory()}/image1.jpg"]`, (err) => {
-            if (err) return done.fail(err);
-            browser.assert.success();
-            done();
-          });
-        });
-
-        it('redirects to the origin album if the delete is successful', function(done) {
-          browser.pressButton('Delete', function(err) {
-            if (err) return done.fail(err);
-
-            browser.assert.success();
-            browser.assert.text('.alert.alert-info', 'Image deleted');
-            browser.assert.url({ pathname: `/image/${agent.getAgentDirectory()}` });
-            done();
-          });
-        });
-
-        it('deletes the image from the file system', function(done) {
-          fs.readdir(`uploads/${agent.getAgentDirectory()}`, (err, files) => {
-            if (err) return done.fail(err);
-            expect(files.length).toEqual(3);
-            expect(files.includes('image1.jpg')).toBe(true);
-
-            browser.pressButton('Delete', function(err) {
+      describe('deleting', function() {
+        describe('owner resource', function() {
+          beforeEach(function(done) {
+            browser.clickLink(`a[href="/image/${agent.getAgentDirectory()}/image1.jpg"]`, (err) => {
               if (err) return done.fail(err);
               browser.assert.success();
-
-              fs.readdir(`uploads/${agent.getAgentDirectory()}`, (err, files) => {
-                if (err) return done.fail(err);
-                expect(files.length).toEqual(2);
-                expect(files.includes('image1.jpg')).toBe(false);
-
-                done();
-              });
+              done();
             });
           });
-        });
-      });
 
-      describe('readable resource', function() {
-        beforeEach(function(done) {
-          browser.visit(`/image/${lanny.getAgentDirectory()}/image1.jpg`, (err) => {
-            if (err) return done.fail(err);
-            browser.assert.success();
-            done();
+          it('redirects to the origin album if the delete is successful', function(done) {
+            browser.pressButton('Delete', function(err) {
+              if (err) return done.fail(err);
+
+              browser.assert.success();
+              browser.assert.text('.alert.alert-info', 'Image deleted');
+              browser.assert.url({ pathname: `/image/${agent.getAgentDirectory()}` });
+              done();
+            });
           });
-        });
 
-        it('does not show a delete button', () => {
-          browser.assert.elements('#delete-image-form', 0);
-        });
+          it('deletes the image from the file system', function(done) {
+            fs.readdir(`uploads/${agent.getAgentDirectory()}`, (err, files) => {
+              if (err) return done.fail(err);
+              expect(files.length).toEqual(3);
+              expect(files.includes('image1.jpg')).toBe(true);
 
-        it('does not delete the image from the file system', function(done) {
-          fs.readdir(`uploads/${lanny.getAgentDirectory()}`, (err, files) => {
-            if (err) return done.fail(err);
-            expect(files.length).toEqual(3);
-            expect(files.includes('lanny1.jpg')).toBe(true);
-
-            request(app)
-              .delete(`/image/${lanny.getAgentDirectory()}/lanny1.jpg`)
-              .set('Cookie', browser.cookies)
-              .end(function(err, res) {
+              browser.pressButton('Delete', function(err) {
                 if (err) return done.fail(err);
-                expect(res.status).toEqual(302);
-                expect(res.header.location).toEqual(`/image/${lanny.getAgentDirectory()}`);
+                browser.assert.success();
 
-                fs.readdir(`uploads/${lanny.getAgentDirectory()}`, (err, files) => {
+                fs.readdir(`uploads/${agent.getAgentDirectory()}`, (err, files) => {
                   if (err) return done.fail(err);
-                  expect(files.length).toEqual(3);
-                  expect(files.includes('lanny1.jpg')).toBe(true);
+                  expect(files.length).toEqual(2);
+                  expect(files.includes('image1.jpg')).toBe(false);
 
                   done();
                 });
               });
+            });
           });
         });
-      });
 
-      describe('unauthorized resource', function() {
-        let troy;
-        beforeEach(function(done) {
-          models.Agent.findOne({ email: 'troy@example.com' }).then(function(result) {
-            troy = result;
-
-            expect(agent.canRead.length).toEqual(1);
-            expect(agent.canRead[0]).not.toEqual(troy._id);
-
-            browser.visit(`/image/${troy.getAgentDirectory()}/somepic.jpg`, function(err) {
+        describe('readable resource', function() {
+          beforeEach(function(done) {
+            browser.visit(`/image/${lanny.getAgentDirectory()}/image1.jpg`, (err) => {
               if (err) return done.fail(err);
+              browser.assert.success();
               done();
             });
-          }).catch(function(error) {
-            done.fail(error);
           });
-        });
 
-        it('redirects home', () => {
-          browser.assert.redirected();
-          browser.assert.url({ pathname: '/'});
-          browser.assert.text('.alert.alert-danger', 'You are not authorized to access that resource');
-        });
+          it('does not show a delete button', () => {
+            browser.assert.elements('#delete-image-form', 0);
+          });
 
-        it('does not delete the image from the file system', function(done) {
-          mkdirp(`uploads/${troy.getAgentDirectory()}`, (err) => {
-            fs.writeFileSync(`uploads/${troy.getAgentDirectory()}/troy1.jpg`, fs.readFileSync('spec/files/troll.jpg'));
-
-            fs.readdir(`uploads/${troy.getAgentDirectory()}`, (err, files) => {
+          it('does not delete the image from the file system', function(done) {
+            fs.readdir(`uploads/${lanny.getAgentDirectory()}`, (err, files) => {
               if (err) return done.fail(err);
-              expect(files.length).toEqual(1);
-              expect(files.includes('troy1.jpg')).toBe(true);
+              expect(files.length).toEqual(3);
+              expect(files.includes('lanny1.jpg')).toBe(true);
 
               request(app)
-                .delete(`/image/${troy.getAgentDirectory()}/troy1.jpg`)
+                .delete(`/image/${lanny.getAgentDirectory()}/lanny1.jpg`)
                 .set('Cookie', browser.cookies)
                 .end(function(err, res) {
                   if (err) return done.fail(err);
                   expect(res.status).toEqual(302);
-                  expect(res.header.location).toEqual('/');
+                  expect(res.header.location).toEqual(`/image/${lanny.getAgentDirectory()}`);
 
-                  fs.readdir(`uploads/${troy.getAgentDirectory()}`, (err, files) => {
+                  fs.readdir(`uploads/${lanny.getAgentDirectory()}`, (err, files) => {
                     if (err) return done.fail(err);
-                    expect(files.length).toEqual(1);
-                    expect(files.includes('troy1.jpg')).toBe(true);
+                    expect(files.length).toEqual(3);
+                    expect(files.includes('lanny1.jpg')).toBe(true);
 
                     done();
                   });
                 });
             });
+          });
+        });
+
+        describe('unauthorized resource', function() {
+          let troy;
+          beforeEach(function(done) {
+            models.Agent.findOne({ email: 'troy@example.com' }).then(function(result) {
+              troy = result;
+
+              expect(agent.canRead.length).toEqual(1);
+              expect(agent.canRead[0]).not.toEqual(troy._id);
+
+              browser.visit(`/image/${troy.getAgentDirectory()}/somepic.jpg`, function(err) {
+                if (err) return done.fail(err);
+                done();
+              });
+            }).catch(function(error) {
+              done.fail(error);
+            });
+          });
+
+          it('redirects home', () => {
+            browser.assert.redirected();
+            browser.assert.url({ pathname: '/'});
+            browser.assert.text('.alert.alert-danger', 'You are not authorized to access that resource');
+          });
+
+          it('does not delete the image from the file system', function(done) {
+            mkdirp(`uploads/${troy.getAgentDirectory()}`, (err) => {
+              fs.writeFileSync(`uploads/${troy.getAgentDirectory()}/troy1.jpg`, fs.readFileSync('spec/files/troll.jpg'));
+
+              fs.readdir(`uploads/${troy.getAgentDirectory()}`, (err, files) => {
+                if (err) return done.fail(err);
+                expect(files.length).toEqual(1);
+                expect(files.includes('troy1.jpg')).toBe(true);
+
+                request(app)
+                  .delete(`/image/${troy.getAgentDirectory()}/troy1.jpg`)
+                  .set('Cookie', browser.cookies)
+                  .end(function(err, res) {
+                    if (err) return done.fail(err);
+                    expect(res.status).toEqual(302);
+                    expect(res.header.location).toEqual('/');
+
+                    fs.readdir(`uploads/${troy.getAgentDirectory()}`, (err, files) => {
+                      if (err) return done.fail(err);
+                      expect(files.length).toEqual(1);
+                      expect(files.includes('troy1.jpg')).toBe(true);
+
+                      done();
+                    });
+                  });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+
+  describe('from index view', () => {
+
+    describe('authenticated', () => {
+      beforeEach(done => {
+
+        stubAuth0Sessions(agent.email, DOMAIN, err => {
+          if (err) done.fail(err);
+
+          // Don't mock too soon. The stub needs some files
+          mockAndUnmock({
+            [`uploads/${agent.getAgentDirectory()}`]: {
+              'image1.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'image2.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'image3.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            },
+            [`uploads/${lanny.getAgentDirectory()}`]: {
+              'lanny1.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'lanny2.jpg': fs.readFileSync('spec/files/troll.jpg'),
+              'lanny3.jpg': fs.readFileSync('spec/files/troll.jpg'),
+            },
+            'public/images/uploads': {}
+          });
+
+          browser.clickLink('Login', err => {
+            if (err) done.fail(err);
+            browser.assert.success();
+            done();
+          });
+        });
+      });
+
+      afterEach(() => {
+        mock.restore();
+      });
+
+      it('renders a form to allow an agent to delete an image', () => {
+        browser.assert.url({ pathname: `/image/${agent.getAgentDirectory()}` });
+        browser.assert.elements('#delete-image-form', 0);
+        browser.assert.elements('.delete-image-form', 3);
+        browser.assert.element(`form[action="/image/${agent.getAgentDirectory()}/image1.jpg?_method=DELETE"]`);
+      });
+
+      describe('deleting', () => {
+        describe('owner resource', () => {
+          beforeEach(() => {
+            browser.assert.url({ pathname: `/image/${agent.getAgentDirectory()}` });
+          });
+
+          it('redirects to the origin album if the delete is successful', done => {
+            browser.pressButton('Delete', err => {
+              if (err) return done.fail(err);
+
+              browser.assert.success();
+              browser.assert.text('.alert.alert-info', 'Image deleted');
+              browser.assert.url({ pathname: `/image/${agent.getAgentDirectory()}` });
+              done();
+            });
+          });
+
+          it('deletes the image from the file system', done => {
+            fs.readdir(`uploads/${agent.getAgentDirectory()}`, (err, files) => {
+              if (err) return done.fail(err);
+              expect(files.length).toEqual(3);
+              expect(files.includes('image1.jpg')).toBe(true);
+              expect(files.includes('image2.jpg')).toBe(true);
+              expect(files.includes('image3.jpg')).toBe(true);
+
+              browser.pressButton('Delete', err => {
+                if (err) return done.fail(err);
+                browser.assert.success();
+
+                fs.readdir(`uploads/${agent.getAgentDirectory()}`, (err, files) => {
+                  if (err) return done.fail(err);
+                  expect(files.length).toEqual(2);
+                  expect(files.includes('image1.jpg')).toBe(true);
+                  expect(files.includes('image2.jpg')).toBe(true);
+                  expect(files.includes('image3.jpg')).toBe(false);
+
+                  done();
+                });
+              });
+            });
+          });
+        });
+
+        describe('readable resource', () => {
+          beforeEach(done => {
+            browser.visit(`/image/${lanny.getAgentDirectory()}`, (err) => {
+              if (err) return done.fail(err);
+              browser.assert.success();
+              done();
+            });
+          });
+
+          it('does not show a delete button', () => {
+            browser.assert.elements('#delete-image-form', 0);
+            browser.assert.elements('.delete-image-form', 0);
+          });
+        });
+
+        describe('unauthorized resource', () => {
+          let troy;
+          beforeEach(done => {
+            models.Agent.findOne({ email: 'troy@example.com' }).then(result => {
+              troy = result;
+
+              expect(agent.canRead.length).toEqual(1);
+              expect(agent.canRead[0]).not.toEqual(troy._id);
+
+              browser.visit(`/image/${troy.getAgentDirectory()}`, err => {
+                if (err) return done.fail(err);
+                done();
+              });
+            }).catch(error => {
+              done.fail(error);
+            });
+          });
+
+          it('redirects home', () => {
+            browser.assert.redirected();
+            browser.assert.url({ pathname: '/'});
+            browser.assert.text('.alert.alert-danger', 'You are not authorized to access that resource');
           });
         });
       });
