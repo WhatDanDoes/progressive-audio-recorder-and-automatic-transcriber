fc80bdf2efa8804d598ca54bb0843a022d1fbee6
Daniel Bidulock <daniel_bidulock@sil.org>
2020-10-09 11:12:06 -0600

    Remove image file system operations for root route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const fs = require('fs');
 const models = require('../models');
 
 const MAX_IMGS = parseInt(process.env.MAX_IMGS);
-
 
 /**
  * This consolidates the functionality required of
@@ -12,53 +10,29 @@ const MAX_IMGS = parseInt(process.env.MAX_IMGS);
  */
 
 function getMainPhotoRoll(page, req, res) {
-  if (req.user) {
-    models.Image.find({ published: true }).populate('photographer').skip(MAX_IMGS * (page - 1)).limit(MAX_IMGS).sort({ updatedAt: 'desc' }).then(images => {
+  models.Image.find({ published: true }).populate('photographer').skip(MAX_IMGS * (page - 1)).limit(MAX_IMGS).sort({ updatedAt: 'desc' }).then(images => {
 
-      let nextPage = page + 1,
-          prevPage = page - 1;
-      if (images.length < MAX_IMGS) {
-        nextPage = 0;
-      }
+    let nextPage = page + 1,
+        prevPage = page - 1;
+    if (images.length < MAX_IMGS) {
+      nextPage = 0;
+    }
 
-      res.render('index', { images: images, messages: req.flash(), agent: req.user, nextPage: nextPage, prevPage: prevPage });
+    res.render('index', { images: images, messages: req.flash(), agent: req.user, nextPage: nextPage, prevPage: prevPage });
 
-    }).catch(err => {
-      req.flash('error', err.message);
-      return res.redirect('/');
-    });
-  }
-  else {
-    fs.readdir('public/images/uploads', (err, files) => {
-      if (err) {
-        return res.render('error', { error: err, message: 'Couldn\'t read upload directory' });
-      }
-
-      files = files.filter(item => (/\.(gif|jpg|jpeg|tiff|png)$/i).test(item)).reverse();
-
-      let nextPage = 0,
-          prevPage = page - 1;
-      if (files.length > MAX_IMGS * page) {
-        nextPage = page + 1;
-        files = files.slice(MAX_IMGS * prevPage, MAX_IMGS * page);
-      }
-
-      if (!nextPage && prevPage) {
-        files = files.slice(MAX_IMGS * prevPage);
-      }
-
-      res.render('index', { images: files, messages: req.flash(), agent: req.user, nextPage: nextPage, prevPage: prevPage });
-    });
-  }
+  }).catch(err => {
+    req.flash('error', err.message);
+    return res.redirect('/');
+  });
 }
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   return getMainPhotoRoll(1, req, res);
 });
 
-router.get('/page/:num', function(req, res, next) {
+router.get('/page/:num', (req, res) => {
   const page = parseInt(req.params.num);
 
   if (page <= 0) {
