37ae796e8a3b19f953c9042793bf3c599d0281e9
Daniel Bidulock <daniel_bidulock@sil.org>
2021-02-18 09:56:30 -0700

    Correct spec description

--- spec/features/imageIndexSpec.js
+++ spec/features/imageIndexSpec.js
@@ -93,7 +93,7 @@ describe('imageIndexSpec', () => {
 
     describe('authorized', () => {
 
-      it('displays an Android deep link with JWT if browser is mobile', done => {
+      it('displays an add-photo form', done => {
         browser.headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'};
         browser.visit(`/image/${agent.getAgentDirectory()}`, err => {
           if (err) return done.fail(err);
