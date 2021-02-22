547d4e7660f65f124197624fddc74bbbdea644db
Daniel Bidulock <daniel_bidulock@sil.org>
2021-02-22 16:22:42 -0700

    Add call to get access to media on device

--- public/scripts/camera.js
+++ public/scripts/camera.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-  const supported = 'mediaDevices' in navigator;
+  var supported = 'mediaDevices' in navigator;
 
   if (supported) {
     // Does this device have a camera?
@@ -9,10 +9,27 @@ document.addEventListener('DOMContentLoaded', function(event) {
          * Swap out basic image upload form
          */
         var section = document.querySelector('.deep-link');
-        section.innerHTML = '<div id="camera-button"><img src="/images/bpe-logo.png"><br>Add photos</div>';
+        section.innerHTML = `
+          <div id="camera-button">
+            <img src="/images/bpe-logo.png"><br>
+            Add photos
+          </div>
+        `;
 
-        section.addEventListener('click', evt => {
-          console.log(evt);
+        section.addEventListener('click', function(evt) {
+          //const player = document.getElementById('player');
+
+          const constraints = {
+            audio: false,
+            video: true,
+          };
+
+          navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+            //player.srcObject = stream;
+            console.log('stream', stream);
+          }).catch(function(err) {
+            console.error(err);
+          });
         });
       }
     }).catch(function(err) {
