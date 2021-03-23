da55926a2e655deb4ce75ff6cb522b4e256e78e9
Daniel Bidulock <daniel_bidulock@sil.org>
2021-03-23 15:14:39 -0600

    Get passing progressive video element test

--- public/scripts/camera.js
+++ public/scripts/camera.js
@@ -9,14 +9,19 @@ document.addEventListener('DOMContentLoaded', function(event) {
          * Swap out basic image upload form
          */
         var section = document.querySelector('.deep-link');
+        var defaultImageForm = section.innerHTML;
         section.innerHTML = `
           <div id="camera-button">
             <img src="/images/bpe-logo.png"><br>
             Add photos
           </div>
+          <video id="player" controls autoplay></video>
+          <canvas id="canvas" width=320 height=240></canvas>
         `;
 
         section.addEventListener('click', function(evt) {
+console.log('evt');
+console.log(evt);
           //const player = document.getElementById('player');
 
           const constraints = {
@@ -25,10 +30,29 @@ document.addEventListener('DOMContentLoaded', function(event) {
           };
 
           navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-            //player.srcObject = stream;
+            section.insertAdjacentHTML =
             console.log('stream', stream);
+
+            const player = document.getElementById('player');
+            const canvas = document.getElementById('canvas');
+            const context = canvas.getContext('2d');
+            const captureButton = document.getElementById('capture');
+
+//            const constraints = {
+//              video: true,
+//            };
+
+//            captureButton.addEventListener('click', () => {
+//              // Draw the video frame to the canvas.
+//              context.drawImage(player, 0, 0, canvas.width, canvas.height);
+//            });
+
+            // Attach the video stream to the video element and autoplay.
+            player.srcObject = stream;
+
           }).catch(function(err) {
             console.error(err);
+            section.innerHTML = defaultImageForm;
           });
         });
       }
