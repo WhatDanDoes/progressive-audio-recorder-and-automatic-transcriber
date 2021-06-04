6774833a717f1c0e2d834a28561c81082973a52d
Daniel Bidulock <daniel_bidulock@sil.org>
2021-06-04 08:54:14 -0600

    Add admin route and clean up

--- routes/agent.js
+++ routes/agent.js
@@ -1,50 +1,51 @@
 'use strict';
 
 const express = require('express');
-const passport = require('passport');
 const router = express.Router();
 const models = require('../models');
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
-const isMobile = require('is-mobile');
 
 /**
  * GET /agent
  */
 router.get('/', function(req, res) {
-  if (!req.isAuthenticated()) { 
+  if (!req.isAuthenticated()) {
     req.flash('error', 'You need to login first');
     return res.redirect('/');
   }
   req.user.getReadables(function(err, readables) {
 
-    // To open deep link with auth token
-    const payload = { email: req.user.email };
-    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1h' });
-
-    models.Agent.find({ email: { $ne: req.user.email } }).sort({ 'updatedAt': 'desc' }).then(activeAgents => {
-      res.render('agent/index', {
-        messages: req.flash(),
-        agent: req.user,
-        readables: readables,
-        token: token,
-        isMobile: isMobile({ ua: req.headers['user-agent'], tablet: true}),
-        activeAgents: activeAgents
-      });
-    }).catch(err => {
-      req.flash('error', err.message);
-      res.render('agent/index', {
-        messages: req.flash(),
-        agent: req.user,
-        readables: readables,
-        token: token,
-        isMobile: isMobile({ ua: req.headers['user-agent'], tablet: true}),
-        activeAgents: []
-      });
+    res.render('agent/index', {
+      messages: req.flash(),
+      agent: req.user,
+      readables: readables,
     });
   });
 });
 
+/**
+ * GET /agent/admin
+ */
+router.get('/admin', function(req, res) {
+  if (!req.isAuthenticated() || req.user.email !== process.env.SUDO) {
+    req.flash('error', 'Unauthorized');
+    return res.redirect('/');
+  }
+
+  models.Agent.find({ email: { $ne: req.user.email } }).sort({ 'updatedAt': 'desc' }).then(activeAgents => {
+    res.render('agent/admin', {
+      messages: req.flash(),
+      agent: req.user,
+      activeAgents: activeAgents
+    });
+  }).catch(err => {
+    req.flash('error', err.message);
+    res.render('agent/admin', {
+      messages: req.flash(),
+      agent: req.user,
+      activeAgents: []
+    });
+  });
+});
 
 
 module.exports = router;
