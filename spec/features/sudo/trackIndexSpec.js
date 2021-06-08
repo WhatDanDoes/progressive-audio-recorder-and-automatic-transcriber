14acd2e3bf6af6ba4919720bc1292f3f40c72782
Daniel Bidulock <daniel_bidulock@sil.org>
2021-06-08 10:24:04 -0600

    Clean up

--- spec/features/sudo/trackIndexSpec.js
+++ spec/features/sudo/trackIndexSpec.js
@@ -146,8 +146,6 @@ describe('sudo trackIndexSpec', () => {
       });
 
       it('displays an add-track form', done => {
-//        browser.headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'};
-        //browser.visit(`/track/${agent.getAgentDirectory()}`, err => {
         browser.clickLink(agent.getAgentDirectory(), err => {
           if (err) return done.fail(err);
 
